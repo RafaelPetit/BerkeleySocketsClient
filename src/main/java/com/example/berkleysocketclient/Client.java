62f456910791dc53eaa1f96fb6a9563d8b7a294a
Rafael Petit <rpetit.dev@gmail.com>
2025-03-12 12:08:50 -0300

    feat: Contrutor cliente

---
+++ src/main/java/com/example/berkleysocketclient/Client.java
@@ -0,0 +1,23 @@
+package com.example.berkleysocketclient;
+
+import java.io.*;
+import java.net.Socket;
+
+public class Client {
+
+    private Socket socket;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+
+    public Client(Socket socket) {
+        try{
+            this.socket = socket;
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao criar o cliente");
+            fecharTudo(socket, bufferedReader, bufferedWriter);
+        }
+    }
+}
